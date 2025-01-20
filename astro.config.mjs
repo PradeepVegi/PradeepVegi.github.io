34c54e0f004b9a06b28cb3501aa0ee4406a185bc
pradeep <pvegired@andrew.cmu.edu>
2025-01-20 12:00:34 -0500

    trying out astro github host tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,21 @@
 import { defineConfig } from 'astro/config';
+// import sitemap from '@astrojs/sitemap';
+// import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-export default defineConfig({});
+const SERVER_PORT = 3000;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const LIVE_URL = 'https://pradeepvegi.github.io';
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes('astro build');
+let BASE_URL = LOCALHOST_URL
+
+if (isBuild) {
+    BASE_URL = LIVE_URL
+}
+
+export default defineConfig({
+    server: { port: SERVER_PORT },
+    site: BASE_URL,
+    // integrations: [tailwind(), sitemap()],
+    site: 'https://PradeepVegi.github.io',
+});
